a90dd535f7a5cbeef391509d9e2be249e91f28d0
wjdqls2499 <wjdqls2499@gmail.com>
2024-11-13 13:24:14 +0900

    4회차 과제

---
+++ 4회차 과제/4회차 과제/4회차 과제.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include<string>
+using namespace std;
+
+class Student {
+    string name;
+    int Id;
+    string major;
+
+public:
+    void getName() {
+        cout << name;
+    }
+    void getId() {
+        cout << Id;
+    }
+    void getMajor() {
+        cout << major;
+    }
+
+    Student() : name(""), Id(0), major("") {
+    }
+
+    Student(string na, int id) :
+        name(na), Id(id), major("컴퓨터공학과") {
+    }
+
+    Student(string na, int id, string ma) :
+        name(na), Id(id), major(ma) {
+    }
+};
+
+int main()
+{
+    
+}
+
+
